26032ad7e35dad0bc97e374d229401fb0bf7b473
rushabh ramani <rushabhramani16@gmail.com>
2024-10-30 17:21:30 +0530

    video 3

--- lib/bloc/counter_event.dart
+++ lib/bloc/counter_event.dart
@@ -1,1 +1,3 @@
-class CounterIncrementEvent {}
+sealed class CounterEvent{}
+final class CounterIncrementEvent extends CounterEvent{}
+final class CounterDecrementEvent extends CounterEvent{}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:statemanagement/bloc/counter_bloc.dart';
+import 'package:statemanagement/bloc/counter_event.dart';
+import 'package:statemanagement/bloc/counter_state.dart';
 
 void main() {
   runApp(const MyApp());
@@ -6,11 +10,12 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      debugShowCheckedModeBanner:false,
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 73, 255, 1)),
         useMaterial3: true,
@@ -23,16 +28,20 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
   final String title;
+
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
+
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  final counterBloc = CounterBloc();
+
+  @override
+  void dispose() {
+    counterBloc.close(); // Add dispose method
+    super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,18 +56,40 @@ class _MyHomePageState extends State<MyHomePage> {
             const Text(
               'You have pushed the button this many times:',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            BlocBuilder<CounterBloc, CounterState>(
+              buildWhen: (previous, current) {
+                return true;
+              },
+              bloc: counterBloc,
+              builder: (context, state) {
+                return Text(
+                  state.count.toString(),
+                  style: Theme.of(context).textTheme.headlineMedium,
+                );
+              }
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end, // Add this to align buttons to the end
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              counterBloc.add(CounterIncrementEvent());
+            },
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+           FloatingActionButton(
+            onPressed: () {
+              counterBloc.add(CounterDecrementEvent());
+            },
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+        ],
+      ),
     );
   }
 }
